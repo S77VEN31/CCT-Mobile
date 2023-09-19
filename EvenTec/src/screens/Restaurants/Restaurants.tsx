e920178c49cc96480f4423a545bee7692d960866
S77VEN31 <stivensg313131@outlook.com>
2023-09-18 23:54:08 -0700

    hotfix: remove week nav bar

--- EvenTec/src/screens/Restaurants/Restaurants.tsx
+++ EvenTec/src/screens/Restaurants/Restaurants.tsx
@@ -1,22 +1,13 @@
 // React
-import { View, ScrollView } from "react-native";
+import { View, Text } from "react-native";
 // Styles
 import { styles } from "./Restaurants.style";
-// Enumerables
-import { RestaurantsConstants } from "../../constants/screens/RestaurantsConstants/RestaurantsConstants";
-// Components
-import RestaurantCard from "../../components/Cards/RestaurantCard/RestaurantCard";
 
 const Restaurants: React.FC = () => {
-  const { restaurants } = RestaurantsConstants;
   return (
-    <ScrollView>
-      <View style={styles.container}>
-        {restaurants.map(({ ...props }, key) => {
-          return <RestaurantCard key={key} {...props} />;
-        })}
-      </View>
-    </ScrollView>
+    <View>
+      <Text>About</Text>
+    </View>
   );
 };
 export default Restaurants;

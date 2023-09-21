dcb1652b4ad026d597c4645510a01c8f5eb7ce2b
S77VEN <stivensg313131@outlook.com>
2023-09-21 00:41:28 -0600

    add: Fonts

--- EvenTec/src/constants/Fonts.ts
+++ EvenTec/src/constants/Fonts.ts
@@ -1,48 +1,49 @@
 import { StyleSheet } from 'react-native';
 
 export const Fonts = StyleSheet.create({
-    concertOneTitle: {
-        fontFamily: 'ConcertOne',
-        fontSize: 32,
-    },
-    concertOneSubtitle: {
-        fontFamily: 'ConcertOne',
-        fontSize: 24,
-    },
     textThin: {
         fontFamily: 'KanitThin',
-        fontSize: 16,
     },
     textExtraLight: {
         fontFamily: 'KanitExtraLight',
-        fontSize: 16,
     },
     textLight: {
         fontFamily: 'KanitLight',
-        fontSize: 16,
     },
     textRegular: {
         fontFamily: 'KanitRegular',
-        fontSize: 16,
     },
     textMedium: {
         fontFamily: 'KanitMedium',
-        fontSize: 16,
     },
     textSemiBold: {
         fontFamily: 'KanitSemiBold',
-        fontSize: 16,
     },
     textBold: {
         fontFamily: 'KanitBold',
-        fontSize: 16,
     },
     textExtraBold: {
         fontFamily: 'KanitExtraBold',
-        fontSize: 16,
     },
     textBlack: {
         fontFamily: 'KanitBlack',
+    },
+    h1: {
+        fontSize: 32,
+    },
+    h2: {
+        fontSize: 24,
+    },
+    h3: {
+        fontSize: 20,
+    },
+    h4: {
+        fontSize: 18,
+    },
+    h5: {
         fontSize: 16,
+    },
+    h6: {
+        fontSize: 14,
     },
 });
